3848f66d53e19dbcc973bec2b58b6ceed0d172cb
tw106 <tw106@cms-r1-31.cms.waikato.ac.nz>
2019-06-07 01:46:14 +1200

    Fixed remainder of bugs - only one remaining is 404 error while accessing /isPrime without a variable. dont have the knowledge to implement

--- app.py
+++ app.py
@@ -21,28 +21,30 @@ def get_hit_count():
 
 def fastIsPrime(num, k=5): #source code: https://tinyurl.com/millerRabinSourceCode
     from random import randint
-    try:
-        n = int(num)
-        if n < 2: return ("False");
-        for p in [2,3,5,7,11,13,17,19,23,29]:
-            if n % p == 0: 
-                cache.sadd(primeSet, n) 
-                return ("True") 
-        s, d = 0, n-1
-        while d % 2 == 0:
-            s, d = s+1, d/2
-        for i in range(k):
-            x = pow(randint(2, n-1), int(d), int(n))
-            if x == 1 or x == n-1: continue
-            for r in range(1, s):
-                x = (x * x) % n
-                if x == 1: return ("False") 
-                if x == n-1: break
-            else: return ("False")
-        cache.sadd(primeSet, n) 
-        return ("True") 
-    except ValueError:
-        return ("ERROR: Not a valid number!")
+    if num is not None:
+        try:
+            n = int(num)
+            if n < 2: return ("False");
+            for p in [2,3,5,7,11,13,17,19,23,29]:
+                if n % p == 0: 
+                    cache.sadd(primeSet, n) 
+                    return ("True") 
+            s, d = 0, n-1
+            while d % 2 == 0:
+                s, d = s+1, d/2
+            for i in range(k):
+                x = pow(randint(2, n-1), int(d), int(n))
+                if x == 1 or x == n-1: continue
+                for r in range(1, s):
+                    x = (x * x) % n
+                    if x == 1: return ("False") 
+                    if x == n-1: break
+                else: return ("False")
+            cache.sadd(primeSet, n) 
+            return ("True") 
+        except ValueError:
+            return ("ERROR: Not a valid number!")
+    else: return ("ERROR")
 
 @app.route('/')
 def hello1():
@@ -51,8 +53,12 @@ def hello1():
 
 @app.route('/isPrime/<num>')
 def hello2(num):
-    count1 = fastIsPrime(num)
-    return 'The number {} is prime: {}\n'.format(num, count1)
+    try:
+        count1 = fastIsPrime(num)
+        return 'The number {} is prime: {}\n'.format(num, count1)
+    except:
+        return 'ERROR: specify a number!'
+
 
 @app.route('/primesStored')
 def hello3():
