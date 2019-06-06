40144a56a72926e0571c760f53f6a422c805f3a1
tw106 <tw106@cms-r1-31.cms.waikato.ac.nz>
2019-06-06 20:43:44 +1200

    Second commit; replaced brute force prime finder with a millerRabin style test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import time
 
 import redis
 from flask import Flask, url_for
+from random import randint
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -18,26 +19,25 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-def get_if_prime(num):
-# If given number is greater than 1 
-# Iterate from 2 to n / 2
-# If num is divisible by any number between 
-# 2 and n / 2, it is not prime./ source: https://tinyurl.com/soucrecode
-#cashe.sadd(primeSet - set numbers, primes)
-
-    number = int(num)
-    if number > 1:      
-        for i in range(2, number//2): 
-            if (number % i) == 0: 
-                returnString = ""
-                return(number, "is not a prime number") 
-                break
-        else: 
-            return (number, "is a prime number")
-            cache.sadd(primeSet, number);
-    else: 
-        return (num, "is not a prime number") 
-
+def fastIsPrime(num, k=5): #source code: https://tinyurl.com/millerRabinSourceCode
+    from random import randint
+    n = int(num)
+    if n < 2: return (n, "is not a prime number")
+    for p in [2,3,5,7,11,13,17,19,23,29]:
+        if n % p == 0: return n == p
+    s, d = 0, n-1
+    while d % 2 == 0:
+        s, d = s+1, d/2
+    for i in range(k):
+        x = pow(randint(2, n-1), int(d), int(n))
+        if x == 1 or x == n-1: continue
+        for r in range(1, s):
+            x = (x * x) % n
+            if x == 1: return (n, "is not a prime number") 
+            if x == n-1: break
+        else: return (n, "is not a prime number")
+    cache.sadd(primeSet, n); 
+    return (n, "is a prime number") 
 
 @app.route('/')
 def hello1():
@@ -46,8 +46,8 @@ def hello1():
 
 @app.route('/isPrime/<num>')
 def hello2(num):
-    count1 = get_if_prime(num)
-    return 'The number {}\n'.format(count1)
+    count1 = fastIsPrime(num)
+    return 'The number is prime: {}\n'.format(count1)
 
 @app.route('/primesStored')
 def hello3():
