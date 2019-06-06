3e90f44acdb7727154951a76fc8b8dce083b50d3
tw106 <tw106@cms-r1-31.cms.waikato.ac.nz>
2019-06-07 00:39:35 +1200

    Fixed bug that stopped 2-29 primes from showing.

--- app.py
+++ app.py
@@ -21,27 +21,28 @@ def get_hit_count():
 
 def fastIsPrime(num, k=5): #source code: https://tinyurl.com/millerRabinSourceCode
     from random import randint
-    n = int(num)
-    if n < 2: return ("False");
-    for p in [2,3,5,7,11,13,17,19,23,29]:
-        if n % p == 0: return n == p
-    s, d = 0, n-1
-    while d % 2 == 0:
-        s, d = s+1, d/2
-    for i in range(k):
-        x = pow(randint(2, n-1), int(d), int(n))
-        if x == 1 or x == n-1: continue
-        for r in range(1, s):
-            x = (x * x) % n
-            if x == 1: return ("False") 
-            if x == n-1: break
-        else: return ("False")
-    cache.sadd(primeSet, n) 
-    return ("True") 
-
-def returnList():
-    rS = ""
-    return (rS);
+    try:
+        n = int(num)
+        if n < 2: return ("False");
+        for p in [2,3,5,7,11,13,17,19,23,29]:
+            if n % p == 0: 
+                cache.sadd(primeSet, n) 
+                return ("True") 
+        s, d = 0, n-1
+        while d % 2 == 0:
+            s, d = s+1, d/2
+        for i in range(k):
+            x = pow(randint(2, n-1), int(d), int(n))
+            if x == 1 or x == n-1: continue
+            for r in range(1, s):
+                x = (x * x) % n
+                if x == 1: return ("False") 
+                if x == n-1: break
+            else: return ("False")
+        cache.sadd(primeSet, n) 
+        return ("True") 
+    except ValueError:
+        return ("ERROR: Not a valid number!")
 
 @app.route('/')
 def hello1():
@@ -51,13 +52,13 @@ def hello1():
 @app.route('/isPrime/<num>')
 def hello2(num):
     count1 = fastIsPrime(num)
-    return 'The number {} is prime!: {}\n'.format(num, count1)
+    return 'The number {} is prime: {}\n'.format(num, count1)
 
 @app.route('/primesStored')
 def hello3():
-    return 'List!: {}\n'.format(cache.smembers(primeSet))
+    return 'List: {}\n'.format(cache.smembers(primeSet))
 
 @app.route('/clear')
 def hello4():
     cache.flushdb()
-    return 'Cleared!!'
+    return 'Cleared!'
